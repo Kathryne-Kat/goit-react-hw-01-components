1364d92f298c416fb55c893aa504fac25906a1fc
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-02-06 23:35:59 +0200

    add one part

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendListItem
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            key={id}
+          ></FriendListItem>
+        );
+      })}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  <li className="item">
+    <span className="status">{isOnline}</span>
+    <img className="avatar" src={avatar} alt="User avatar" width="48" />
+    <p className="name">{name}</p>
+  </li>;
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
---
+++ src/components/TransactionHistory.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <TransactionHistoryItem
+              key={id}
+              type={type}
+              amount={amount}
+              currency={currency}
+            ></TransactionHistoryItem>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  item: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+const TransactionHistoryItem = ({ type, amount, currency }) => {
+  return (
+    <tr>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
+    </tr>
+  );
+};
+
+TransactionHistoryItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
