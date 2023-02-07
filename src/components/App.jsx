cfd2c02861b29b962426e16142bc8c14ca1c78bd
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-02-07 17:55:51 +0200

    add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import user from 'data/user.json';
-import { Profile } from './Profile';
+import { Profile } from './Profile/Profile';
 import data from 'data/data.json';
 import { Statistics } from './Statistics';
 import friends from 'data/friends.json';
@@ -16,7 +16,7 @@ export const App = () => {
         flexDirection: 'column', //не было
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        // fontSize: 40,
         color: '#010101',
       }}
     >
--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -17,27 +17,29 @@ export const FriendList = ({ friends }) => {
   );
 };
 
-FriendList.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired,
-    })
-  ),
-};
+// FriendList.propTypes = {
+//   friends: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       avatar: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       isOnline: PropTypes.bool.isRequired,
+//       id: PropTypes.number.isRequired,
+//     })
+//   ),
+// };
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
-  <li className="item">
-    <span className="status">{isOnline}</span>
-    <img className="avatar" src={avatar} alt="User avatar" width="48" />
-    <p className="name">{name}</p>
-  </li>;
+  return (
+    <li className="item">
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
 };
 
-FriendListItem.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-};
+// FriendListItem.propTypes = {
+//   avatar: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   isOnline: PropTypes.bool.isRequired,
+// };
